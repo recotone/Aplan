97f1fc2d579aa8cf6c037accdac9f6eb32ca64cb
yh <yangheng@chk.com>
2017-02-03 16:08:58 +0800

    init

---
+++ aplan-app/src/main/java/com/aplan/boot/APP.java
@@ -0,0 +1,19 @@
+/**
+ * @ProjectName: aplan-app
+ * @Description: 本内容仅限于招商云途内部使用，禁止转发.
+ */
+package com.aplan.boot;
+
+
+/**
+ * <p></p>
+ * @author yh Feb 2, 2017 11:02:44 PM
+ * @version V1.0   
+ * @date Feb 2, 2017 11:02:44 PM
+ */
+public class APP {
+	
+	 public static void main(String[] args) {
+		
+	}
+}
